c18ffe917ddddc1946f4e5a010b537f3a4be7723
Angela Chou <angnchou@gmail.com>
2018-11-07 12:25:15 -0800

    complete functional stack shared behavior

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,31 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size = Object.keys(storage).length;
+  var size = 0;
+  var count = 0;
 
 
   // Implement the methods below
   someInstance.push = function(value) {
     //increment size inside push since push is what changes the size
     size ++;
+    storage[count] = value;
+    count ++;
   };
 
   someInstance.pop = function() {
+    size --;
+
+    for (var key in storage) {
+      var keys = Object.keys(storage);
+      var lastKeys = keys.slice(keys.length-1);
+      console.log(lastKeys)
+      return storage[lastKeys];
+      delete storage[lastKeys];
+    }
+    //lastKeys[lastKeys.length];
+    
+    
   };
 
   someInstance.size = function() {
@@ -22,9 +37,10 @@ var Stack = function() {
     //   return Object.keys(storage).length;
     //can just return length like below
     //return Object.keys(storage).length;
+    if (size < 0) {
+      return 0;
+    }
     return size;
-
-
   };
 
   return someInstance;
