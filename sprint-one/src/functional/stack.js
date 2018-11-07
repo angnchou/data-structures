43996cb9f5dfcfc4454ee099e4321aa8c36f2f13
Angela Chou <angnchou@gmail.com>
2018-11-07 11:44:07 -0800

    start functional stack with TDD

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,28 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = Object.keys(storage).length;
+
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //increment size inside push since push is what changes the size
+    size ++;
   };
 
   someInstance.pop = function() {
   };
 
   someInstance.size = function() {
+    // if (Object.keys(storage).length === 0) {
+    //   return 0;
+    // } else {
+    //   return Object.keys(storage).length;
+    //can just return length like below
+    //return Object.keys(storage).length;
+    return size;
+
+
   };
 
   return someInstance;
