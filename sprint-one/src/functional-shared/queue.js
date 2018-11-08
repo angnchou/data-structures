6a997a6201ab2a926a48f8bd23aa02876576b383
Angela Chou <angnchou@gmail.com>
2018-11-07 23:13:11 -0800

    complete functional shared stack and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,44 @@
+
+// var extend = function(obj1, obj2) {
+//     for (var key in obj2) {
+//       obj1[key] = obj2[key];
+//     }
+//     return obj1;
+// };
+
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.storage = { start: 0, end: 0, size: 0};
+  console.log(someInstance)
+  return _.extend(someInstance, queueMethods);
+
+
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.storage.end] = value;
+    this.storage.end ++;
+    this.storage.size ++;
+  },
+
+  dequeue: function() {
+    if (this.storage.size <= 0) {
+      return undefined;
+    }
+    var firstValue = this.storage[this.storage.start];
+    delete this.storage[this.storage.start];
+    this.storage.start ++;
+    this.storage.size --;
+    console.log(firstValue)
+    return firstValue;
+  },
+
+  size: function() {
+    return this.storage.size;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,66 @@
-var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var extend = function(obj1, obj2) {
+    for (var key in obj2) {
+      obj1[key] = obj2[key];
+    }
+    return obj1;
 };
 
-var stackMethods = {};
+var Stack = function(value) {
+  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+  // but try not not reference your old code in writing the new style.
+
+   //var storage = {};
+   //storage.count = 0;
+  return extend({count: 0}, stackMethods);
+};
 
 
+
+var stackMethods = {
+  push: function(value) {
+  //console.log(this, 'push function')
+   this[this.count] = value;
+   this.count ++;
+   // console.log(this)
+
+  },
+  size: function() {
+    //var size;
+    //console.log(this.count, 'size function')
+    return this.count;
+  },
+
+  pop: function() {
+    if (this.count <= 0) {
+      return undefined; // guard clause so no delete happens if []
+    }
+    this.count--;
+    var lastValue = this[this.count];
+    delete this[this.count];
+    return lastValue;    
+  }  
+};
+
+console.log(typeof Stack.push);
+//console.log(obj);
+
+
+
+// stackMethods.push = function(value) {
+//  storage[count] = value;
+//  size ++;
+//  count ++;
+
+// }
+
+
+
+// stackMethods.pop = function() {
+//   size --;
+// }
+
+
+// stackMethods.size = function() {
+//   return size;
+
+// }
