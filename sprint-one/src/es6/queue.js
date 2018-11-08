271fede84d8b26b94d7b7d9b0eb032ae1f9d61d8
Angela Chou <angnchou@gmail.com>
2018-11-08 08:48:35 -0800

    es6 queue and stack in progress  Please enter the commit message for your changes. Lines starting

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,65 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.storage.size = 0;
+    this.storage.start = 0;
+    this.storage.end = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.storage.end] = value;
+    this.storage.size ++;
+    this.storage.end ++;
+    console.log(this.storage.size, 'push');
+  }
+
+  dequeue() {
+    if (this.storage.size <= 0) {
+      return undefined;
+    }
+    
+    var lastValue = this.storage[this.storage.start];
+    delete this.storage.start;
+    this.storage.size -- ;
+    this.storage.start ++;
+    return lastValue;
+  }
+
+  size() {
+    return this.storage.size;
+  }
+
+
 }
+
+
+// var Queue = function() {
+//   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+//   // but try not not reference your old code in writing the new style.
+  
+//   this.storage = {start: 0, end: 0, size: 0};
+//   this.storage.size = 0;
+// };
+
+// Queue.prototype.enqueue = function(value) {
+//     this.storage[this.storage.end] = value;
+//     this.storage.size ++;
+//     this.storage.end ++;
+//   },
+
+// Queue.prototype.dequeue = function() {
+//     if (this.storage.size <= 0) {
+//       return undefined;
+//     }
+ 
+//     var firstValue = this.storage[this.storage.start];
+//     delete this.storage[this.storage.start];
+//     this.storage.start ++;
+//     this.storage.size --;
+//     return firstValue;
+//   },
+
+// Queue.prototype.size = function() {
+//     return this.storage.size;
+// };
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,32 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.storage.size = 0;
   }
 
-}
+  push(value) {
+    this.storage[this.storage.size] = value;
+    this.storage.size ++;
+    console.log(this.storage.size, 'push');
+  }
+
+  pop() {
+    if (this.storage.size <= 0) {
+      return undefined;
+    }
+    
+    var lastValue = this.storage[this.storage.size];
+    delete this.storage.size;
+    this.storage.size -- ;
+    return lastValue;
+  }
+
+  size() {
+    return this.storage.size;
+  }
+
+
+};
+
+var box = new Stack();
