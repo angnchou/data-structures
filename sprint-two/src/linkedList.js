e9bfc9a0c1bbafd70eb00fec4d6ba61db04f7f1f
angnchou <angnchou@gmail.com>
2018-11-08 16:07:14 -0800

    "adds newNodes to list"

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,22 +1,31 @@
 var LinkedList = function() {
   var list = {};
-  list.head = null;
-  list.tail = null;
+  list.head = null; //{value: 4, next: null}
+  list.tail = null; //{value: 4, next: null}
+
   
 
   list.addToTail = function(value) {
     //list.tail++;
-    var newNode = Node(value);
-    list.newNode;
-    list.tail = newNode;
+    var newNode = Node(value); //4
+    if (list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail = newNode;//5
+      list.head.next = newNode; //{value: 4, next: {value:5, next: null}}
+       
+    }
     //list.tail = newNode.next //null
-    return list.tail;
+    //return list.tail;
+ 
     
   };
 
   list.removeHead = function() {
     //console.log(list.head.value);
-    list.head = null;
+    console.log(list)
+    list.head.next = null;
   };
 
   list.contains = function(target) {
@@ -26,10 +35,10 @@ var LinkedList = function() {
   return list;
 };
 
-var Node = function(value) {
+var Node = function(value) { //4
   var node = {};
 
-  node.value = value;
+  node.value = value; //4
   node.next = null;
   
   return node;
