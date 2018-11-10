bd199c9e563289b10d6e1da628b0dcf460244fb5
angnchou <angnchou@gmail.com>
2018-11-09 21:57:55 -0800

    "hashTable in progress"

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,14 +3,19 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  // console.log(this._storage)
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // console.log(this._storage)
+  this._storage.set(index, v);
+  console.log(this._storage)
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.get(index)
 };
 
 HashTable.prototype.remove = function(k) {
