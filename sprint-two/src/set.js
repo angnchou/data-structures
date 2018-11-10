4baebc1559c7c6c5236de8542abad2f8700129e7
angnchou <angnchou@gmail.com>
2018-11-09 18:48:08 -0800

    "set complete"

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,22 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = {}; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  this.storage[item] = item;
 };
 
 setPrototype.contains = function(item) {
+  var keys = Object.keys(this.storage);
+  return keys.includes(item);
 };
 
 setPrototype.remove = function(item) {
+  delete this.storage[item];
 };
 
 /*
