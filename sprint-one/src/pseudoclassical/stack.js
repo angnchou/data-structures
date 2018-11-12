98eecc7782e69a62ac2a1c2f09fed29e98e0b609
Angela Chou <angnchou@gmail.com>
2018-11-11 19:17:30 -0800

    update pseudoclassical stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,27 +1,55 @@
+// var Stack = function() {
+//   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
+//   // but try not not reference your old code in writing the new style.
+
+//   this.storage = {};
+//   this.storage.size = 0;
+
+// };
+//   Stack.prototype.push = function(value) {
+//     this.storage[this.storage.size] = value;
+//     this.storage.size ++;
+//   }
+
+//   Stack.prototype.pop = function() {
+//     if (this.storage.size <= 0) {
+//      return undefined;
+//     }
+//     this.storage.size --;
+//     var lastValue = this.storage[this.storage.size];
+//     delete this.storage[this.storage.size];
+//     return lastValue;
+//   }
+//   Stack.prototype.size = function() {
+//     return this.storage.size;
+//   }
+
+
+
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 
   this.storage = {};
-  this.storage.size = 0;
+  this.size = 0;
 
 };
   Stack.prototype.push = function(value) {
-    this.storage[this.storage.size] = value;
-    this.storage.size ++;
+    this.storage[this.size] = value;
+    this.size ++;
   }
 
   Stack.prototype.pop = function() {
-    if (this.storage.size <= 0) {
+    if (this.size <= 0) {
      return undefined;
     }
-    this.storage.size --;
-    var lastValue = this.storage[this.storage.size];
-    delete this.storage[this.storage.size];
+    this.size --;
+    var lastValue = this.storage[this.size];
+    delete this.storage[this.size];
     return lastValue;
   }
   Stack.prototype.size = function() {
-    return this.storage.size;
+    return this.size;
   }
 
 
